b8ab757179e0d83acd64bdf40ff3e2c608f299a0
Shane Celis <shane.celis@gmail.com>
2026-08-22 01:45:57 -0700

    feat: Let's return an inclusive range.

--- src/lib.rs
+++ src/lib.rs
@@ -144,7 +144,10 @@ impl Iterator for Bresenham {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        if self.x >= self.x1 {
+        // "The endpoints of the [bresenham] line are the pixels at (x0, y0) and
+        // (x1, y1) where the first coordinate of the pair is the column and the
+        // second is the row."
+        if self.x > self.x1 {
             return None;
         }
 
@@ -175,7 +178,7 @@ mod tests {
         let bi = Bresenham::new((0, 1), (6, 4));
         let res: Vec<_> = bi.collect();
 
-        assert_eq!(res, [(0, 1), (1, 1), (2, 2), (3, 2), (4, 3), (5, 3)])
+        assert_eq!(res, [(0, 1), (1, 1), (2, 2), (3, 2), (4, 3), (5, 3), (6, 4)])
     }
 
     #[test]
@@ -183,7 +186,7 @@ mod tests {
         let bi = Bresenham::new((6, 4), (0, 1));
         let res: Vec<_> = bi.collect();
 
-        assert_eq!(res, [(6, 4), (5, 4), (4, 3), (3, 3), (2, 2), (1, 2)])
+        assert_eq!(res, [(6, 4), (5, 4), (4, 3), (3, 3), (2, 2), (1, 2), (0, 1)])
     }
 
     #[test]
@@ -191,7 +194,7 @@ mod tests {
         let bi = Bresenham::new((2, 3), (5, 3));
         let res: Vec<_> = bi.collect();
 
-        assert_eq!(res, [(2, 3), (3, 3), (4, 3)]);
+        assert_eq!(res, [(2, 3), (3, 3), (4, 3), (5, 3)]);
     }
 
     #[test]
@@ -199,6 +202,6 @@ mod tests {
         let bi = Bresenham::new((2, 3), (2, 6));
         let res: Vec<_> = bi.collect();
 
-        assert_eq!(res, [(2, 3), (2, 4), (2, 5)]);
+        assert_eq!(res, [(2, 3), (2, 4), (2, 5), (2, 6)]);
     }
 }
